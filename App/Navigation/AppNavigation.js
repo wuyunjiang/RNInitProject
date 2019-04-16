4a8b98261465788ed88c701015c2ee10176e64db
wuyunjiang <wuyunjiang@arhieason.com>
2019-04-16 14:59:55 +0800

    使用 ignite g 命令生成Home、Personal、Login屏幕及相应redux、saga文件

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,10 +1,16 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation'
+import HomeScreen from '../Containers/HomeScreen'
+import LoginScreen from '../Containers/LoginScreen'
+import PersonalScreen from '../Containers/PersonalScreen'
 import LaunchScreen from '../Containers/LaunchScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = createStackNavigator({
+  HomeScreen: { screen: HomeScreen },
+  LoginScreen: { screen: LoginScreen },
+  PersonalScreen: { screen: PersonalScreen },
   LaunchScreen: { screen: LaunchScreen }
 }, {
   // Default config for all screens
